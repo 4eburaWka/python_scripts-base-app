2c5552f893ccc2eb21fe2bed50077aebf21264a4
Литвинюк Тимофей <12755@user.domain>
2025-08-26 10:59:36 +0300

    remove trash

--- models/app.py
+++ models/app.py
@@ -8,9 +8,3 @@ from sqlalchemy.orm import DeclarativeBase, MappedColumn, mapped_column
 
 class Base(DeclarativeBase):
     id: MappedColumn[str] = mapped_column(UUID(as_uuid=False), primary_key=True)
-
-
-class User(Base):
-    created_at: MappedColumn[datetime.datetime] = mapped_column(DateTime(), server_default=func.now())
-    tiktok: MappedColumn[str] = mapped_column(VARCHAR(40))
-    instagram: MappedColumn[str] = mapped_column(VARCHAR(40))
