45452213c20457dd2c1ae03b83558795e284865e
4eburaWka <prikpasa14@gmail.com>
2025-09-18 09:51:39 +0300

    update configs

--- configs/config.py
+++ configs/config.py
@@ -2,6 +2,8 @@ import os
 
 from dotenv import load_dotenv
 
+from configs import constants
+
 
 dotenv_path = ".env"
 if os.path.exists(dotenv_path):
@@ -16,7 +18,15 @@ def getenv(name: str) -> str:
     return env
 
 
-ENV = getenv("ENV")
+def parse_env(env: str):
+    match env:
+        case constants.LOCAL_ENV | constants.DEV_ENV | constants.PROD_ENV:
+            return env
+        case _:
+            raise ValueError("Env must be one of: local, dev, prod")
+
+
+ENV = parse_env(getenv("ENV"))
 
 # DATABASE
 DB_NAME = getenv("DB_NAME")
@@ -24,3 +34,9 @@ DB_USER = getenv("DB_USER")
 DB_PASSWORD = getenv("DB_PASSWORD")
 DB_HOST = getenv("DB_HOST")
 DB_PORT = getenv("DB_PORT")
+
+# REDIS
+REDIS_HOST = getenv("REDIS_HOST")
+REDIS_PORT = getenv("REDIS_PORT")
+REDIS_PASSWORD = getenv("REDIS_PASSWORD")
+REDIS_DB = getenv("REDIS_DB")
